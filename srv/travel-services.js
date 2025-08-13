370ea15736d2626a369b93885c5f56c0b1fc515b
Pratibha Jawale <pratibhapsarode@gmail.com>
2025-08-13 10:19:45 +0000

    changes for new trial account

--- srv/travel-services.js
+++ srv/travel-services.js
@@ -1,33 +1,4 @@
-//This is a code for destination connection for workflow call
-// const { executeHttpRequest } = require('@sap-cloud-sdk/http-client');
 
-// module.exports = async function (srv) {
-//   srv.on('startTravelWorkflow', async (req) => {
-//     try {
-//       const travelData = JSON.parse(req.data.travelData);
-
-//       const response = await executeHttpRequest(
-//         { destinationName: 'BPA_API_DEST' }, // Your destination name
-//         {
-//           method: 'POST',
-//           url: '/workflow/rest/v1/workflow-instances',
-//           headers: {
-//             'Content-Type': 'application/json'
-//           },
-//           data: {
-//             definitionId: "us10.6440fac5trial.travelexpenses.tavelExpensesProcess",
-//             context: travelData
-//           }
-//         }
-//       );
-
-//       return `Workflow triggered. ID: ${response.data.id}`;
-//     } catch (err) {
-//       console.error("BPA Trigger Error:", err.message);
-//       return `Error: ${err.message}`;
-//     }
-//   });
-// };
 
 const axios = require('axios');
 
@@ -45,7 +16,7 @@ module.exports = async function (srv) {
         {
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vNjQ0MGZhYzV0cmlhbC5hdXRoZW50aWNhdGlvbi51czEwLmhhbmEub25kZW1hbmQuY29tL3Rva2VuX2tleXMiLCJraWQiOiJkZWZhdWx0LWp3dC1rZXktZjUzZGQxYTE4YyIsInR5cCI6IkpXVCIsImppZCI6ICJENUZZOWZyRDljY2lKV1I1NUR4Y24xM1BLQW05akJKVTh5VXNyMXdsK3hVPSJ9.eyJqdGkiOiI4OTVlYWI1NDQ3ZDI0NjUwYjlhYTJjYmYzZDI0MWU1ZiIsImV4dF9hdHRyIjp7ImVuaGFuY2VyIjoiWFNVQUEiLCJzdWJhY2NvdW50aWQiOiJmY2VhNWY1OS02ZDIwLTQ1YTUtYjBhZS1hYTQ5NmZjMDkxYzciLCJ6ZG4iOiI2NDQwZmFjNXRyaWFsIiwic2VydmljZWluc3RhbmNlaWQiOiIxNzQ5MzQwYy03NjJhLTQ5YTktYTczNi0wNDQyMTg3MjRmOTAifSwic3ViIjoic2ItMTc0OTM0MGMtNzYyYS00OWE5LWE3MzYtMDQ0MjE4NzI0ZjkwIWI0NDYzNjh8eHN1YWEhYjQ5MzkwIiwiYXV0aG9yaXRpZXMiOlsidWFhLnJlc291cmNlIl0sInNjb3BlIjpbInVhYS5yZXNvdXJjZSJdLCJjbGllbnRfaWQiOiJzYi0xNzQ5MzQwYy03NjJhLTQ5YTktYTczNi0wNDQyMTg3MjRmOTAhYjQ0NjM2OHx4c3VhYSFiNDkzOTAiLCJjaWQiOiJzYi0xNzQ5MzQwYy03NjJhLTQ5YTktYTczNi0wNDQyMTg3MjRmOTAhYjQ0NjM2OHx4c3VhYSFiNDkzOTAiLCJhenAiOiJzYi0xNzQ5MzQwYy03NjJhLTQ5YTktYTczNi0wNDQyMTg3MjRmOTAhYjQ0NjM2OHx4c3VhYSFiNDkzOTAiLCJncmFudF90eXBlIjoiY2xpZW50X2NyZWRlbnRpYWxzIiwicmV2X3NpZyI6IjdiMWVjMWIiLCJpYXQiOjE3NTM5NDA2NjMsImV4cCI6MTc1Mzk4Mzg2MywiaXNzIjoiaHR0cHM6Ly82NDQwZmFjNXRyaWFsLmF1dGhlbnRpY2F0aW9uLnVzMTAuaGFuYS5vbmRlbWFuZC5jb20vb2F1dGgvdG9rZW4iLCJ6aWQiOiJmY2VhNWY1OS02ZDIwLTQ1YTUtYjBhZS1hYTQ5NmZjMDkxYzciLCJhdWQiOlsidWFhIiwic2ItMTc0OTM0MGMtNzYyYS00OWE5LWE3MzYtMDQ0MjE4NzI0ZjkwIWI0NDYzNjh8eHN1YWEhYjQ5MzkwIl19.P3IA-KtRxPIJPuHM4B79UQJszio98PO89CNhSBlktz_kHN9TiVLYa9z5ZE0uxh-nODWrAZUadOmUmbuC6CnO_xUVFk-CWjdDJEhyBN83hIeum38dwwPp_g850hz0WXn6C-Sqm1LA2YyTywS6CHxo1S682f67ogZ89c2LcF75XyVt3G1Gem-cL-SNjNbX60VDmTdW1UyFbmsiVuEvYOcN2NL2k-bnu5mJPrSAQxcN-1jI329bK60eXR6f2E8MKUlUv3W-pVD0vId6ZStfjN-jLpJ7XbvWb7P4ukSx7CIB3m952UP1JDB4Y8rc_J6WDuUHxCj8HvHOhvRc-yv7SS-rEQ'
+            'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vODNmMzU3YTB0cmlhbC5hdXRoZW50aWNhdGlvbi51czEwLmhhbmEub25kZW1hbmQuY29tL3Rva2VuX2tleXMiLCJraWQiOiJkZWZhdWx0LWp3dC1rZXktMDA5MzA4ZWU5ZiIsInR5cCI6IkpXVCIsImppZCI6ICJ2VFYxR3ptSFlkM2R4WFhIWGxSUThQSU96NGZva1MrU2lrbDAyd2JpQ1Z3PSJ9.eyJqdGkiOiIwNzk5MTFjN2VhMjM0OWM5OWE0ZDBiNzMxY2QxNWZlZiIsImV4dF9hdHRyIjp7ImVuaGFuY2VyIjoiWFNVQUEiLCJzdWJhY2NvdW50aWQiOiI0MzhjMTVkNS01ZTdhLTQwNWUtODIzYS0wZTcwYmRjMjM4MDMiLCJ6ZG4iOiI4M2YzNTdhMHRyaWFsIiwic2VydmljZWluc3RhbmNlaWQiOiJmMjA3YzY5Ni1mYTlkLTQ1NDEtOTY5NS05OTAwNGNmZTdlYTkifSwic3ViIjoic2ItZjIwN2M2OTYtZmE5ZC00NTQxLTk2OTUtOTkwMDRjZmU3ZWE5IWI0OTgyNzN8eHN1YWEhYjQ5MzkwIiwiYXV0aG9yaXRpZXMiOlsidWFhLnJlc291cmNlIl0sInNjb3BlIjpbInVhYS5yZXNvdXJjZSJdLCJjbGllbnRfaWQiOiJzYi1mMjA3YzY5Ni1mYTlkLTQ1NDEtOTY5NS05OTAwNGNmZTdlYTkhYjQ5ODI3M3x4c3VhYSFiNDkzOTAiLCJjaWQiOiJzYi1mMjA3YzY5Ni1mYTlkLTQ1NDEtOTY5NS05OTAwNGNmZTdlYTkhYjQ5ODI3M3x4c3VhYSFiNDkzOTAiLCJhenAiOiJzYi1mMjA3YzY5Ni1mYTlkLTQ1NDEtOTY5NS05OTAwNGNmZTdlYTkhYjQ5ODI3M3x4c3VhYSFiNDkzOTAiLCJncmFudF90eXBlIjoiY2xpZW50X2NyZWRlbnRpYWxzIiwicmV2X3NpZyI6IjMwMDIwZmQwIiwiaWF0IjoxNzU1MDc0NzU5LCJleHAiOjE3NTUxMTc5NTksImlzcyI6Imh0dHBzOi8vODNmMzU3YTB0cmlhbC5hdXRoZW50aWNhdGlvbi51czEwLmhhbmEub25kZW1hbmQuY29tL29hdXRoL3Rva2VuIiwiemlkIjoiNDM4YzE1ZDUtNWU3YS00MDVlLTgyM2EtMGU3MGJkYzIzODAzIiwiYXVkIjpbInVhYSIsInNiLWYyMDdjNjk2LWZhOWQtNDU0MS05Njk1LTk5MDA0Y2ZlN2VhOSFiNDk4MjczfHhzdWFhIWI0OTM5MCJdfQ.Cvo5dpyzch3yURH0L1EYbb5aE-SS9faJ4GU2iSckFfcA9AdsrZLW3xwPqMj9DURp2iVoCWXEWlMBGG1VWWCFxeXr1OUDSkD_LaMzA1bLZcAKEo8DQx9ZNSv7QLyIw58wAIssto4oZPpLI7VHmFCAOP7aZ0A1RzOCiUq94rcBafSSYds4Bh-NvGv-YQzN8DCHWNeijExCOWyUQWGBJlYcM-b7eqd0_K_12Ze2ioYoART8E9BXI1zeHwvzxmI1sQxu57jWhmgE3U9arM-WPKGXRn68JaV38xs-P9jaam5F0dIhSNyvoClF2bE-F_uTnsL19__hSe3UMG1TqTdOGV_QTQ'
           }
         }
       );
